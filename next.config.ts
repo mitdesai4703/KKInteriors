2c073d6dddfb9c19c088925535c9e07f7c1fac89
Mit Desai <mitdesai4703@gmail.com>
2025-11-19 06:11:39 +0530

    commit changes

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+
+  experimental: {
+    optimizeCss: false,
+  },
 };
 
 export default nextConfig;
